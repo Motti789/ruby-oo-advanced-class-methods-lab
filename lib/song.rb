ce7902ce526c357789281b188af33f8577dc03cb
Motti789 <mchillywilly@gmail.com>
2021-07-22 14:24:30 -0400

    commit tree 45e591d69ab827e46571718c85886840f0fc3438

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -40,4 +42,35 @@ class Song
   def self.alphabetical
     self.all.sort_by{|s| s.name}
   end  
+
+  def self.new_from_filename(filename)
+    parsed_info = filename.split(" - ")
+    artist_name = parsed_info[0]
+    song = parsed_info[1]
+    song_arr = song.split(".")
+    song_name = song_arr[0] 
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  #binding.pry
+
 end
